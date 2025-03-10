ec4e886d507a07ac191eacbc7dd63461d531b461
Atulyajain306 <atulyajain306@gmail.com>
2025-03-11 02:30:34 +0530

    latest commit

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose"
 const dbconnection=()=>{
    mongoose
-   .connect(process.env.MONGODB_URI)
+   .connect(process.env.MONGODB_URI ,{
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000 
+  })
    .then(()=>{console.log("MongoDB Connnected")}) 
    .catch(err => console.log(err.message));  
 }
