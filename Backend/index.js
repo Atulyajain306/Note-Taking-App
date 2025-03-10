a41f105841abbc2e4e6a85a068eade05ed5ccfc7
Atulyajain306 <atulyajain306@gmail.com>
2025-03-11 01:33:01 +0530

    new change

--- Backend/index.js
+++ Backend/index.js
@@ -24,18 +24,22 @@ import Audio from "./models/Audio.js";
 dotenv.config();
 const app=express()
 app.use(cors({
-    origin: ["https://note-taking-app-94lz.vercel.app"],
+    origin: "https://note-taking-app-94lz.vercel.app",
     methods: ["GET","POST","PUT","DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
 }))
-app.options("*", (req, res) => {
+app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "https://note-taking-app-94lz.vercel.app");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     res.header("Access-Control-Allow-Credentials", "true");
-    return res.sendStatus(200);
-  });
+    next();
+});
+
+app.options("*", (req, res) => {
+    res.status(200).send();
+});
  app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads",express.static("uploads"));
