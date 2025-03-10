0184edfe64aab1d90531b58ad5a74f9dd4844dfe
Atulyajain306 <atulyajain306@gmail.com>
2025-03-10 05:48:39 +0530

    final

--- Backend/index.js
+++ Backend/index.js
@@ -27,6 +27,7 @@ app.use(express.json());
 app.use(cors({
     origin: "https://note-taking-app-frontend-jet.vercel.app", // Allow only frontend to access
     methods: "GET,POST",
+    allowedHeaders: "Content-Type, Authorization",
     credentials: true
  }));
  app.use((req, res, next) => {
