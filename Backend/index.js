6a37b0fd08f421ddad6642dd13732dc4a81e4c42
Atulyajain306 <atulyajain306@gmail.com>
2025-03-10 06:01:16 +0530

    qqq

--- Backend/index.js
+++ Backend/index.js
@@ -23,23 +23,15 @@ import User from "./models/User.js";
 import Audio from "./models/Audio.js";
 dotenv.config();
 const app=express()
-app.use(express.json());
 app.use(cors({
     origin: "https://note-taking-app-frontend-jet.vercel.app", // Allow only frontend to access
     methods: "GET,POST",
     allowedHeaders: "Content-Type, Authorization",
     credentials: true
  }));
- app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://note-taking-app-frontend-jet.vercel.app");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    if (req.method === "OPTIONS") {
-        return res.sendStatus(200);
-    }
-    next();
-});
-
+ 
+ app.use(express.json());
+ app.options("*", cors());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads",express.static("uploads"));
 app.use(cookieParser());
