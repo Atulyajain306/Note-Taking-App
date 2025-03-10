5ff808be5904e0b09e63da14cdc14be6d027b9bb
Atulyajain306 <atulyajain306@gmail.com>
2025-03-10 05:38:10 +0530

    new

--- Backend/index.js
+++ Backend/index.js
@@ -29,6 +29,16 @@ app.use(cors({
     methods: "GET,POST",
     credentials: true
  }));
+ app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://note-taking-app-frontend-jet.vercel.app");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads",express.static("uploads"));
 app.use(cookieParser());
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,15 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{  
-   proxy:{
-    '/api':{
-      target:'http://localhost:3000',
-      changeOrigin: true,
-      secure: false,
-      rewrite: (path) => {
-        return path.replace(/^\/api/, '');
-      }
-    }
-   }
+   proxy:{}
   }
 })
