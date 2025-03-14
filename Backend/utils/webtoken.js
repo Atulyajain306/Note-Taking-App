8320190382be9509f183fcdb70b82e5a9844f159
Atulyajain306 <atulyajain306@gmail.com>
2025-03-14 19:28:38 +0530

    New

--- Backend/utils/webtoken.js
+++ Backend/utils/webtoken.js
@@ -1,10 +1,10 @@
 import jwt from "jsonwebtoken";
 export const generatetokenandcookie=(userId,res)=>{
-      const token=jwt.sign({userId},process.env.JWT_KEY,{expiresIn:"24h",
+      const token=jwt.sign({userId},process.env.JWT_KEY,{expiresIn:"3d",
 
       })
       res.cookie("jwt",token,{
-         maxAge:24*60*60*1000,
+         maxAge:3*24*60*60*1000,
          httpOnly:true,
          sameSite:"strict"
       });
